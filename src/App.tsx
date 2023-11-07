78b597f269e4aa9f30d4a649b1510c94f2f88068
eshaan <eshaanbagga@gmail.com>
2023-11-07 15:20:45 +0530

    done with date selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import DateSelector from "./components/dateSelector/DateSelector";
 
 function App() {
+  const [selectedDateRange, setSelectedDateRange] = useState<{ start: Date; end: Date }>({
+    start: new Date("2015-07-01"),
+    end: new Date("2015-07-01"),
+  });
+
+  console.log(selectedDateRange);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Date Range Selector</h1>
+      <DateSelector selectedDateRange={selectedDateRange} onDateRangeChange={setSelectedDateRange} />
     </div>
   );
 }
---
+++ src/components/dateSelector/DateSelector.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './DateSelector.css'; // Import your CSS file
+
+interface DateSelectorProps {
+  selectedDateRange: { start: Date; end: Date };
+  onDateRangeChange: (range: { start: Date; end: Date }) => void;
+}
+
+const DateSelector: React.FC<DateSelectorProps> = ({ selectedDateRange, onDateRangeChange }) => {
+  const handleStartDateChange = (date: Date) => {
+    onDateRangeChange({ start: date, end: selectedDateRange.end });
+  };
+
+  const handleEndDateChange = (date: Date) => {
+    onDateRangeChange({ start: selectedDateRange.start, end: date });
+  };
+
+  return (
+    <div className="date-selector">
+      <h2>Date Range Selector</h2>
+      <div className="date-picker">
+        <label>Start Date:</label>
+        <DatePicker
+          selected={selectedDateRange.start}
+          onChange={handleStartDateChange}
+          selectsStart
+          startDate={selectedDateRange.start}
+          endDate={selectedDateRange.end}
+        />
+      </div>
+      <div className="date-picker">
+        <label>End Date:</label>
+        <DatePicker
+          selected={selectedDateRange.end}
+          onChange={handleEndDateChange}
+          selectsEnd
+          startDate={selectedDateRange.start}
+          endDate={selectedDateRange.end}
+          minDate={selectedDateRange.start}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DateSelector;
